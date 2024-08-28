039c75e4efa8dc2eed89a7ed91d3769a7e92c45b
Gladness M <karicece@gmail.com>
2024-08-28 22:46:11 +0300

    COMMIT

--- home.php
+++ home.php
@@ -14,29 +14,6 @@ if (isset($_SESSION['patient_id']) && isset($_SESSION['username'])) {
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <!-- <h1>Hello, <?php echo $_SESSION['username']; ?> Welcome! </h1> 
-    
-
-    <p>Your records:</p>
-    <ul>
-        <?php
-        // require_once 'db_connect.php';
-        // $patient_id = $_SESSION['patient_id'];
-
-        // $sql = "SELECT * FROM patient_records WHERE patient_id=?";
-        // $stmt = $mysqli->prepare($sql);
-        // $stmt->bind_param("i", $patient_id);
-        // $stmt->execute();
-        // $result = $stmt->get_result();
-
-        // while ($record = $result->fetch_assoc()) {
-        //    echo "<li> Record ID: " . htmlspecialchars($record['record_id']) . "-" . htmlspecialchars($record['record_details']) . "</li>";
-        // }
-        ?>
-    </ul>
-    <a href="download.php">Download your Records as PDF</a><br>
-    <br><a href="Logout.php">Logout</a>  -->
-
     <div class="sidebar">
         <div class="logo">
             <a href="home.php" class="logo-link">
